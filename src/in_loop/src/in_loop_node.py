cec041955e3d10f4be27c7c72755baf1d4ebf6af
kaustubhsridhar <kaysri2007@gmail.com>
2020-05-20 13:39:09 -0400

    Update in_loop_node.py

--- src/in_loop/src/in_loop_node.py
+++ src/in_loop/src/in_loop_node.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys; sys.dont_write_bytecode = True # to prevent creation of .pyc files
 import rospy
 from std_msgs.msg import String
 from in_loop.msg import in_control
